645492253f947783da998f8703a8fb721b50b8af
gabriel soares <gabrielsantossoares18@gmail.com>
2022-09-29 13:18:01 -0300

    upload

---
+++ projeto_pet_shop/pet_shop/php/dados.php
@@ -0,0 +1,12 @@
+$nome= $_POST["dono"];
+$_POST["endereco"];
+$_POST["cep"];
+$_POST["email_dono"];
+$_POST["tel_dono"];
+
+
+echo "Nome: ". $nome <br>;
+echo "Seu endereço: ". $_POST["endereco"]<br>;
+echo "Seu cep: ". $_POST["cep"]<br>;
+echo "Seu email: ". $_POST["email_dono"]<br>;
+echo "Seu telefone: ". $_POST["tel_dono"]<br>;
